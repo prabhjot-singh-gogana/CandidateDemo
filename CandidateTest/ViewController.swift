126d0219945b95e212b559de27f4e790c44429ae
Prabhjot <pgogana@promptu.com>
2022-08-20 22:19:19 +1000

    Initial Commit

---
+++ CandidateTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CandidateTest
+//
+//  Created by Prabhjot Singh Gogana on 20/8/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
